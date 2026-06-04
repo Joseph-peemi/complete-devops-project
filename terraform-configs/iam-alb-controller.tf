864799034f89d0327367c51ce2f9b8e725358933
Joseph-Peemi <peemijoe9522@gmail.com>
2026-06-04 17:54:56 +0100

    modified:   .vscode/settings.json         modified:   complete-devops-project-time-printer/templates/service.yaml         modified:   complete-devops-project-time-printer/values.yaml         modified:   terraform-configs/iam-alb-controller.tf         modified:   terraform-configs/provider.tf

--- terraform-configs/iam-alb-controller.tf
+++ terraform-configs/iam-alb-controller.tf
@@ -1,17 +1,18 @@
 # Data sources
 data "aws_caller_identity" "current" {}
 
-data "aws_eks_cluster" "main" {
-  name = var.cluster_name
-}
-
-data "aws_iam_openid_connect_provider" "eks" {
-  url = data.aws_eks_cluster.main.identity[0].oidc[0].issuer
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
 }
 
 locals {
   account_id = data.aws_caller_identity.current.account_id
-  oidc_url   = replace(data.aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")
+
+  oidc_url = replace(
+    module.eks.cluster_oidc_issuer_url,
+    "https://",
+    ""
+  )
 }
 
 # Trust policy
@@ -20,8 +21,11 @@ data "aws_iam_policy_document" "alb_controller_trust" {
     effect = "Allow"
 
     principals {
-      type        = "Federated"
-      identifiers = ["arn:aws:iam::${local.account_id}:oidc-provider/${local.oidc_url}"]
+      type = "Federated"
+
+      identifiers = [
+        "arn:aws:iam::${local.account_id}:oidc-provider/${local.oidc_url}"
+      ]
     }
 
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -46,26 +50,28 @@ resource "aws_iam_role" "alb_controller" {
   assume_role_policy = data.aws_iam_policy_document.alb_controller_trust.json
 }
 
-# ALB controller policy - using latest v2.11.0
+# ALB controller policy
 resource "aws_iam_policy" "alb_controller" {
   name   = "AWSLoadBalancerControllerIAMPolicy"
   policy = file("${path.module}/iam_policy.json")
 }
 
-# Attach policy to role
+# Attach policy
 resource "aws_iam_role_policy_attachment" "alb_controller" {
   role       = aws_iam_role.alb_controller.name
   policy_arn = aws_iam_policy.alb_controller.arn
 }
 
-# Annotate the Kubernetes service account
+# Kubernetes service account
 resource "kubernetes_service_account_v1" "alb_controller" {
   metadata {
     name      = "aws-load-balancer-controller"
     namespace = "kube-system"
+
     annotations = {
       "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller.arn
     }
+
     labels = {
       "app.kubernetes.io/component" = "controller"
       "app.kubernetes.io/name"      = "aws-load-balancer-controller"
--- terraform-configs/provider.tf
+++ terraform-configs/provider.tf
@@ -9,13 +9,14 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
+
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
