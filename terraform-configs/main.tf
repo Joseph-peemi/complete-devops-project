ebc9e762666255cf051ee8feaeb742072c45b811
Joseph-Peemi <peemijoe9522@gmail.com>
2026-06-04 19:25:02 +0100

    modified:   terraform-configs/main.tf

--- terraform-configs/main.tf
+++ terraform-configs/main.tf
@@ -47,4 +47,15 @@ module "vpc" {
   private_subnets    = var.private_subnets
   public_subnets     = var.public_subnets
   enable_nat_gateway = true
+
+  # Required for ALB controller to discover subnets
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
 }
